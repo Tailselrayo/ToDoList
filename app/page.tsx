21259f2edf513ae55aea79596014d599c7c00925
tailselrayo <rodtailselrayo@gmail.com>
2023-07-10 10:32:11 +0200

    feat(app): added local memory

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
 "use client";
 import { Todolist } from "@/components/Todolist";
-import { TaskType } from "@/types/TaskType";
+import { useStoredTodolist } from "@/hooks/useStoredTodolist";
 import { AppShell, Group, TextInput, Button, Stack, NativeSelect } from "@mantine/core";
-import { useInputState, useListState } from "@mantine/hooks";
+import { useInputState } from "@mantine/hooks";
 import { randomBytes } from "crypto";
-import { todo } from "node:test";
-import { FormEvent } from "react";
+import { FormEvent} from "react";
 
 export default function Home() {  //default est à utiliser pour les pages
   const [task, setTask] = useInputState('');
   const [priority, setPriority] = useInputState<PriorityType>("minor");
-  const [toDoList, handlers] = useListState<TaskType>([]);
+  const {toDoList, handlers} = useStoredTodolist();
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
     if (task.trim() && !toDoList.filter((elem) => elem.description === task).length) {
-      handlers.append({
+      handlers.add({
         description: task, 
         done: false, 
         priority: priority,
@@ -24,35 +23,6 @@ export default function Home() {  //default est à utiliser pour les pages
       setTask('');    //<=> task = '' en plus de rafraichir les components
     }
     
-  }
-
-  const applyOnId = (newTask: TaskType) => {
-    handlers.applyWhere(
-      (elem) => elem.id === newTask.id,
-      () => newTask
-    );
-  }
-
-  const onChange = (task: TaskType) => {
-    applyOnId({...task, done: !task.done});
-  }
-
-  const onDelete = (task: TaskType) => {
-    handlers.filter((elem) => elem.id !== task.id)
-  }
-
-  const onEdit = (task: TaskType, e:string) => {
-    applyOnId({...task, description: e});
-  }
-
-  const onToggleEdit = (task: TaskType) => {
-    if (!toDoList.filter((elem) => elem.isEditing).length || task.isEditing) {
-      applyOnId({...task, isEditing: !task.isEditing})
-    }
-  }
-
-  const priorityChange = (task: TaskType, e: PriorityType) => {
-    applyOnId({...task, priority: e})
   }
 
   return (
@@ -88,12 +58,12 @@ export default function Home() {  //default est à utiliser pour les pages
           </Group>
         </form>
         <Todolist
-          onChange={onChange}
-          onDelete={onDelete}
+          onChange={handlers.change}
+          onDelete={handlers.delete}
           tasks={toDoList}
-          onEdit={onEdit}
-          priorityChange={priorityChange}
-          onToggleEdit={onToggleEdit}
+          onEdit={handlers.edit}
+          priorityChange={handlers.priorityChange}
+          onToggleEdit={handlers.toggleEdit}
         />
       </Stack>
     </AppShell>
