eb22257feb385e7e04734beb64a7cbbc9918f768
tailselrayo <rodtailselrayo@gmail.com>
2023-07-12 12:19:08 +0200

    fix(home):display bug; style(TODOlist):button mobile support

--- components/TodolistCard.tsx
+++ components/TodolistCard.tsx
@@ -16,41 +16,43 @@ export function TodolistCard(props: TodolistCardProps) {
     const theme = useMantineTheme();
 
     return (
-        <GenericTransition>
-            <HoverCard width={100} withArrow keepMounted zIndex={300 + 10 * props.index}>
-                <HoverCard.Target>
-                    <Link href={`/todolist/${props.index}`} style={{ textDecoration: "none" }}>
-                        <Card h={150} bg={theme.fn.linearGradient(135, ...props.color)}>
-                            <Stack>
-                                <Title ta="center">{props.name}</Title>
-                            </Stack>
+        <Box style={{zIndex: 300 - 10 * props.index, position: "relative"}}>
+            <GenericTransition>
+                <HoverCard width={100} withArrow shadow="lg">
+                    <HoverCard.Target>
+                        <Link href={`/todolist/${props.index}`} style={{ textDecoration: "none" }}>
+                            <Card h={150} bg={theme.fn.linearGradient(135, ...props.color)}>
+                                <Stack>
+                                    <Title ta="center">{props.name}</Title>
+                                </Stack>
 
-                        </Card>
-                    </Link>
-                </HoverCard.Target>
-                <HoverCard.Dropdown display={props.isHidden? "none" : "block"} bg="dark.2">
-                    <Group position="center">
-                        <Box>
-                            <Group position="right">
-                                <ScaleButton
-                                    onClick={() => props.onEdit(props)}
-                                    isOver={true}
-                                    tooltipLabel={"Edit list display"}
-                                    color={"dark"}
-                                    icon={<IconPencil />}
-                                />
-                                <ScaleButton
-                                    onClick={() => props.onRemove(props.id)}
-                                    isOver={true}
-                                    tooltipLabel={"Remove list"}
-                                    color={"dark"}
-                                    icon={<IconTrash />}
-                                />
-                            </Group>
-                        </Box>
-                    </Group>
-                </HoverCard.Dropdown>
-            </HoverCard>
-        </GenericTransition>
+                            </Card>
+                        </Link>
+                    </HoverCard.Target>
+                    <HoverCard.Dropdown display={props.isHidden ? "none" : "block"} bg="gray.2" style={{border: 0}}>
+                        <Group position="center">
+                            <Box>
+                                <Group position="right">
+                                    <ScaleButton
+                                        onClick={() => props.onEdit(props)}
+                                        isOver={true}
+                                        tooltipLabel={"Edit list display"}
+                                        color={"dark"}
+                                        icon={<IconPencil />}
+                                    />
+                                    <ScaleButton
+                                        onClick={() => props.onRemove(props.id)}
+                                        isOver={true}
+                                        tooltipLabel={"Remove list"}
+                                        color={"dark"}
+                                        icon={<IconTrash />}
+                                    />
+                                </Group>
+                            </Box>
+                        </Group>
+                    </HoverCard.Dropdown>
+                </HoverCard>
+            </GenericTransition>
+        </Box>
     )
 }
